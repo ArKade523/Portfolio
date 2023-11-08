50f81bf74e2ae7fd60bb6cead6f8e2815e7e7853
Kade Angell <ponmlk523@gmail.com>
2023-11-08 11:52:54 -0700

    added favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,12 @@
 	import Footer from './Footer.svelte';
 	import './general-styles.css';
 	import './card-styles.css';
+	import Logo from '$lib/images/Logo.svg';
 </script>
+
+<svelte:head>
+	<link rel="favicon" type="image/svg+xml" href={Logo} />
+</svelte:head>
 
 <div class="app">
 	<div class="content-wrapper">
